7e16df89b07965f02589c8bbf22e58b7991db30c
hardhus <ahmetcemiltoprakk@gmail.com>
2025-03-31 03:31:28 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ echo "Cleaning up old PM2 processes..."
 pm2 delete battleship-server --silent || exit 1
 
 echo "Starting the app with PM2..."
-PORT=3015 pm2 start ./dist/server.js --name "battleship-server" || exit 1
+pm2 start ./dist/server.js --name "battleship-server" --env PORT=3015 || exit 1
 
 echo "Configuring PM2 to restart on boot..."
 pm2 startup || exit 1
